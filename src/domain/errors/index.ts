517c8b8ec4cd65312d2bb8a5b423ef54bb06acaf
FaustinJ <faustin.jacquemain@humtap.com>
2025-10-13 13:36:05 +0200

    Clean startup with templates

--- src/domain/errors/index.ts
+++ src/domain/errors/index.ts
@@ -1,27 +1,3 @@
-import { ConflictError, NotFoundError, UnprocessableContentError } from "@core/errors";
-
-export class HelloWorldModelNotFoundError extends NotFoundError {
-    constructor() {
-        super('HELLOWORLD_NOT_FOUND');
-    }
-}
-
-export type InvalidHelloWorldModelUsernameErrorDetails = {
-    reason: 'MISSING',
-} | {
-    reason: 'TOO_LONG' | 'TOO_SHORT',
-    got: number,
-    expected: number,
-}
-
-export class InvalidHelloWorldModelUsernameError extends UnprocessableContentError {
-    constructor(reason: InvalidHelloWorldModelUsernameErrorDetails) {
-        super('INVALID_HELLOWORLD_USERNAME', reason);
-    }
-}
-
-export class AlreadyTakenHelloWorldModelUsernameError extends ConflictError {
-    constructor() {
-        super('ALREADY_TAKEN_HELLOWORLD_USERNAME');
-    }
-}
+/**
+ * implements business errors here (see BaseAppError)
+ */
